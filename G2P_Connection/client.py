69615210f5d6cee2a80dff7d84446f710800c8d1
Raulbl01 <raulbl01@ucm.es>
2022-04-22 03:12:08 +0200

    intento poner marcador con texto en archivo client

--- G2P_Connection/client.py
+++ G2P_Connection/client.py
@@ -8,22 +8,26 @@ HEIGHT = 600
 
 WHITE = (255,255,255)
 
+pg.init()
 screen = pg.display.set_mode((WIDTH,HEIGHT))
 pg.display.set_caption("Caption")
 
-        
+font_name = pg.font.match_font('arial')
+def draw_text(surf, text, size, x, y):
+	font = pg.font.Font(font_name, size)
+	text_surface = font.render(text, True, (0,0,0))
+	text_rect = text_surface.get_rect()
+	text_rect.midtop = (x, y)
+	surf.blit(text_surface, text_rect)
+
+
 def display(screen, player1, player2):
     screen.fill(WHITE)
     player1.draw(screen)
     player2.draw(screen)
+	draw_text(screen, f'score: ({player2.score},{player1.score})', width/2, height/2)
     pg.display.update()
     
-    for b in player1.bullets:
-        b.draw(screen)
-    for b in player2.bullets:
-        b.draw(screen)
-    pg.display.update()
-
 def main():
     running = True
     n = Network()
