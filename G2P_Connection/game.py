2d96bf0cb306665aef7d63540b5c508d528fb182
Daniel Saiz <danisaiz@ucm.es>
2022-04-22 22:45:51 +0200

    cambios estéticos

--- G2P_Connection/game.py
+++ G2P_Connection/game.py
@@ -3,20 +3,23 @@ import pygame as pg
 WIDTH = 300
 HEIGHT = 600
 
-GREEN = (0,255,0)
-P_COLOR1 = (5,123,219)
-P_COLOR2 =  (0,0,0)
+P_COLOR1 = (6,62,199)
+P_COLOR1B = (0,247,247)
+P_COLOR2 = (16,131,13)
+P_COLOR2B =  (0,255,0)
 
 class Game():
     def __init__(self, id):
         self.id = id
         self.players = [Player(0,0,50,50,P_COLOR1), Player(0,550,50,50,P_COLOR2)]
-        self.ready = False
-        self.p1_wins = False
-        self.p2_wins = False
-    
-    def connected(self):
-        return self.ready
+# =============================================================================
+#         self.ready = False
+#         self.p1_wins = False
+#         self.p2_wins = False
+#     
+#     def connected(self):
+#         return self.ready
+# =============================================================================
     
 class Player(pg.sprite.Sprite):
     def __init__(self, x, y, w, h, c):
@@ -27,14 +30,17 @@ class Player(pg.sprite.Sprite):
         self.width = w
         self.height = h
         self.color = c
+        if self.color == P_COLOR1:
+            self.colorb = P_COLOR1B
+        else:
+            self.colorb = P_COLOR2B
         self.rect = (x,y,w,h)
         self.speed = 3
         self.bullets = pg.sprite.Group()
         
-
     def draw(self, win):
         pg.draw.rect(win, self.color, self.rect)
-        pg.draw.rect(win, GREEN, self.rect,2) #hitbox
+        pg.draw.rect(win, self.colorb, self.rect,2) #hitbox
         for b in self.bullets:
             b.draw(win)
     
@@ -52,6 +58,10 @@ class Player(pg.sprite.Sprite):
         self.bullets.add(bullet)
     
     def update(self):
+        if self.x+self.width >= WIDTH:
+            self.x = WIDTH-self.width
+        if self.x <= 0:
+            self.x = 0
         self.rect = (self.x, self.y, self.width, self.height)
         self.bullets.update()
                   
@@ -60,14 +70,15 @@ class Bullet(pg.sprite.Sprite):
         pg.sprite.Sprite.__init__(self)
         if y == 550: 
             self.side = 1
+            self.color = P_COLOR2
         else:
             self.side = 0
+            self.color = P_COLOR1
         self.x = x+20
         if self.side == 1:
             self.y = y-26
         else:
-            self.y = y+51
-        self.color = GREEN 
+            self.y = y+51 
         self.rect = (x,y,10,20)
         self.speed = -4
              
