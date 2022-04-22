2b3a6f1ae31480a46a95c00c491fffb6580ace1e
Daniel Saiz <danisaiz@ucm.es>
2022-04-22 11:39:09 +0200

    añadido fondo

--- G2P_Connection/README.txt
+++ G2P_Connection/README.txt
@@ -1,1 +1,7 @@
+INTRO
 Esta carpeta está dedicada a las diferentes versiones que vamos a implementar. Estas versiones son una simplificación de la parte visual y lógica del juego con el fin de poder compartir las cosas entre los diferentes jugadores.
+
+INSTRUCCIONES
+1. Ejecutar server.py
+2. Ejecutar client.py para añadir un nuevo jugador. Cada dos jugadores se crea una nueva sala.
+network.py y game.py son archivos que usan los dos primeros.
--- G2P_Connection/client.py
+++ G2P_Connection/client.py
@@ -10,19 +10,19 @@ WHITE = (255,255,255)
 
 pg.init()
 screen = pg.display.set_mode((WIDTH,HEIGHT))
-pg.display.set_caption("Caption")
-
+pg.display.set_caption("HIT!")
+background = pg.image.load("big.jpg").convert()
+background_rect = background.get_rect() 
 font_name = pg.font.match_font('arial')
 def draw_text(surf, text, size, x, y):
 	font = pg.font.Font(font_name, size)
-	text_surface = font.render(text, True, (0,0,0))
+	text_surface = font.render(text, True, WHITE)
 	text_rect = text_surface.get_rect()
 	text_rect.midtop = (x, y)
 	surf.blit(text_surface, text_rect)
 
 
 def display(screen, player1, player2):
-    screen.fill(WHITE)
     player1.draw(screen)
     player2.draw(screen)
     draw_text(screen, f'score: ({player2.score},{player1.score})', 15, WIDTH/2, HEIGHT/2)
@@ -40,7 +40,7 @@ def main():
     n = Network()
     p = n.get_p()
     clock = pg.time.Clock()
-    
+        
     while running:
         
         clock.tick(FPS)
@@ -64,6 +64,7 @@ def main():
                     b.kill()
         p.move()
         display(screen, p, p2)
+        screen.blit(background, background_rect)
         
 if __name__ == "__main__":
     main()
